8666533f2f8cee83eb05b8733705a81bfa083759
Ramesh <ramesh@repo.com>
2020-02-28 22:04:55 +0530

    Initial Commit

---
+++ src/main/java/com/task/StringConvert.java
@@ -0,0 +1,11 @@
+/*package com.task;
+import org.apache.commons.lang.StringUtils;
+
+public class StringConvert {
+	public String camelConversion(String str)
+	
+	return org.apache.commons.lang3.text.WordUtils.capitalizeFully(String str);
+	
+
+}
+*/
